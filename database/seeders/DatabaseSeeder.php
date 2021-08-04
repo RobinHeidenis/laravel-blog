b56a5f90aaff1beb2f434b65e0620c7c0bf79493
Robin Heidenis <robin.heidenis@gmail.com>
2021-08-04 21:53:28 +0200

    added laravel-idea-helper for autocompletion functions, added db seeder, added users to posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,37 +16,61 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create(['name' => 'Robin Heidenis']);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $hobbies = Category::create([
+            'name' => 'Hobbies',
+            'slug' => 'hobbies'
+        ]);
+
         Post::create([
             'title' => 'My First Post',
             'slug' => 'my-first-post',
+            'excerpt' => '<p>This is my very first post, come and look!</p>',
             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus, deserunt dicta enim expedita id itaque labore magnam modi numquam omnis, perferendis porro quasi, rerum sapiente sit sunt veniam voluptate.</p>',
-            'excerpt' => 'This is my very first post, come and look!',
-            'category_id' => 1
+            'category_id' => $work->id,
+            'user_id' => $user->id
         ]);
 
         Post::create([
             'title' => 'My Second Post',
             'slug' => 'my-second-post',
+            'excerpt' => '<p>This is my second post, come and look!</p>',
             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus, deserunt dicta enim expedita id itaque labore magnam modi numquam omnis, perferendis porro quasi, rerum sapiente sit sunt veniam voluptate.</p>',
-            'excerpt' => 'This is my second post, come and look!',
-            'category_id' => 2
+            'category_id' => $personal->id,
+            'user_id' => $user->id
         ]);
 
         Post::create([
             'title' => 'My Third Post',
             'slug' => 'my-third-post',
+            'excerpt' => '<p>This is my third post, come and look!</p>',
             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus, deserunt dicta enim expedita id itaque labore magnam modi numquam omnis, perferendis porro quasi, rerum sapiente sit sunt veniam voluptate.</p>',
-            'excerpt' => 'This is my third post, come and look!',
-            'category_id' => 0
+            'category_id' => $hobbies->id,
+            'user_id' => $user->id
         ]);
 
         Post::create([
             'title' => 'My Fourth Post',
             'slug' => 'my-fourth-post',
+            'excerpt' => '<p>This is my latest post, come and look!</p>',
             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus, deserunt dicta enim expedita id itaque labore magnam modi numquam omnis, perferendis porro quasi, rerum sapiente sit sunt veniam voluptate.</p>',
-            'excerpt' => 'This is my latest post, come and look!',
-            'category_id' => 0
+            'category_id' => $personal->id,
+            'user_id' => $user->id
         ]);
     }
 }
