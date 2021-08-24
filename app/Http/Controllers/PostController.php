d6dd1e0003ace7adb29a0c7e85a5131296039cca
Robin Heidenis <robin.heidenis@gmail.com>
2021-08-24 21:44:42 +0200

    added filtering, changed routes, added to controller, extracted component with controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,25 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Contracts\View\View;
-use Illuminate\Database\Eloquent\Builder;
 
 class PostController extends Controller
 {
     public function index(): View
     {
-
-
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
-            'categories' => Category::all(),
-            'currentCategory' => Category::firstWhere('slug', request('category'))
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get()
         ]);
     }
 
-    public function show(Post $post): View {
-        return view('post', ['post' => $post]);
+    public function show(Post $post): View
+    {
+        return view('posts.show', ['post' => $post]);
     }
 }
