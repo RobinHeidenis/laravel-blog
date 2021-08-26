fd6fda84e166ca385b3fab1073c3cd7d9deee55d
Robin Heidenis <robin.heidenis@gmail.com>
2021-08-26 23:51:35 +0200

    made comments dynamic

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -13,16 +14,6 @@ class Post extends Model
     protected $fillable = ['title', 'slug', 'excerpt', 'body', 'category_id', 'user_id'];
 
     protected $with = ['category', 'author'];
-
-    public function category(): BelongsTo
-    {
-        return $this->belongsTo(Category::class);
-    }
-
-    public function author(): BelongsTo
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
 
     public function scopeFilter($query, array $filters)
     {
@@ -40,5 +31,20 @@ class Post extends Model
         $query->when($filters['author'] ?? false, fn($query, $author) => $query
             ->whereHas('author', fn($query) => $query
                 ->where('username', $author)));
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
