a30b5ebc78e385b70053287961504ba058101827
Robin Heidenis <robin.heidenis@gmail.com>
2021-08-26 22:49:21 +0200

    added table migration and updated foreign key constraints

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -61,10 +61,6 @@ use App\Models\Post;
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
                     <x-post-comment/>
-                    <x-post-comment/>
-                    <x-post-comment/>
-                    <x-post-comment/>
-                    <x-post-comment/>
                 </section>
             </article>
         </main>
