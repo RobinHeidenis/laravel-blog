6f11eac45b738d73c33b63b0bd83d469f21f4e23
Robin Heidenis <robin.heidenis@gmail.com>
2021-08-26 21:17:57 +0200

    added login page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,11 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
-    public function destroy() {
+    public function create(): Factory|View|Application
+    {
+        return view('sessions.create');
+    }
+
+    public function  store() {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if(auth()->attempt($attributes)) {
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        throw ValidationException::withMessages(['email' => 'Your provided credentials could not be verified.']);
+    }
+
+    public function destroy(): Redirector|Application|RedirectResponse
+    {
         auth()->logout();
 
         return redirect('/')->with('success', 'Goodbye!');
